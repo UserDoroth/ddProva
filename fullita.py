443e53a4c580bf523ae21d952b73cd09ebd99e90
UserDoroth <dorot.h.yqroui.llardi68@gmail.com>
2025-05-21 18:58:58 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -11,8 +11,8 @@ from bs4 import BeautifulSoup
 import time
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://userdoroth-userforkprox.hf.space"     # non mettere lo / finale al link
+MFPPSW = "777"
 
 # Constants
 REFERER = "forcedtoplay.xyz"
